03ff6264888d5f0675b80bccded552cf81015d17
riedhampattel <riedhampattel.tops@gmail.com>
2025-05-30 10:59:30 +0530

    updated

---
+++ C programming/Array/1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	int roll[5] = {1,2,3,4,5};
+	printf("%d",roll[2]);
+	return 0;
+}
---
+++ C programming/Array/2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={1,2,3};
+	printf("\nSize of a = %d",sizeof(a));
+	printf("\n%d",a[2]);
+	return 0;
+}
